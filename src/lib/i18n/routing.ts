021e401b19fef22ddc374cb4b09ac9ee048f8eb2
Allan Nascimento <alcristi.sto@gmail.com>
2025-12-13 06:36:11 -0300

    refactor(i18n): organize i18n structure and messages

--- src/lib/i18n/routing.ts
+++ src/lib/i18n/routing.ts
@@ -2,14 +2,11 @@ import { createNavigation } from 'next-intl/navigation';
 import { defineRouting } from 'next-intl/routing';
  
 export const routing = defineRouting({
-  // A list of all locales that are supported
-  locales: ['en', 'de'],
+  locales: ['en', 'pt'],
  
-  // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'pt'
 });
  
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
+
 export const {Link, redirect, usePathname, useRouter} =
   createNavigation(routing);
